806f6babaff262d05208ea4939f95726c0cfa816
Jared Khan <jaredkhan@me.com>
2018-01-24 23:01:01 +0000

    Add swift-ast dependency

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/sharplet/Regex.git", from: "1.1.0"),
         .package(url: "https://github.com/kareman/SwiftShell.git", from: "4.0.0"),
-        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.18.1")
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.18.1"),
+        .package(url: "https://github.com/yanagiba/swift-ast.git", from: "0.3.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,7 +30,7 @@ let package = Package(
         ),
         .target(
             name: "CaramelFramework",
-            dependencies: ["Regex", "SwiftShell", "SourceKittenFramework"]
+            dependencies: ["Regex", "SwiftShell", "SourceKittenFramework", "SwiftAST"]
         ),
         .testTarget(
             name: "CaramelFrameworkTests",
